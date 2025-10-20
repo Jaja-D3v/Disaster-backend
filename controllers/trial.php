30e1f053024bd83518febdfc79dad75408ef6bf8
Jared | Dev <jaredabrera44@gmail.com>
2025-10-20 15:55:40 +0800

    asdhbashdvasdh

--- controllers/trial.php
+++ controllers/trial.php
@@ -1,47 +1,33 @@
+<?php
+require_once "../models/resetAccount.php";
+require __DIR__ . '/../vendor/autoload.php';
+require_once "../utils/rateLimiter.php";
+require_once "../config/db.php";
+
+use Dotenv\Dotenv;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+$database = new Database();
+$conn = $database->connect();
+
+date_default_timezone_set('Asia/Manila');
+
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $data = json_decode(file_get_contents("php://input"), true);
     $email = trim($data["email"]);
-    $type = $data["type"] ?? "link"; // 🔹 "link" or "code"
+    $type = isset($data["type"]) ? $data["type"] : "forgot_password"; // default type
 
     $userModel = new User();
     $user = $userModel->findByEmail($email);
 
     $rateLimiter = new RateLimiter($conn);
-    $rateLimiter->checkLimit($email, 'forgot_password');
+    $rateLimiter->checkLimit($email, "forgot_password"); // use type for rate limiting
 
     if ($user) {
-        if ($type === "code") {
-            // 🔹 Generate 6-digit verification code
-            $code = rand(100000, 999999);
-            $expires = date("Y-m-d H:i:s", strtotime("+5 minutes"));
-
-            // Store the code in DB (you can reuse your reset_tokens table)
-            $userModel->deleteResetTokenByEmail($email);
-            $userModel->createResetToken($email, $code, $expires);
-
-            $body = "
-                Hi {$user['username']},<br><br>
-                Your verification code is: <b>$code</b><br><br>
-                This code will expire in 5 minutes.
-            ";
-        } else {
-            // 🔹 Default: Token-based reset link
-            $token = bin2hex(random_bytes(16));
-            $expires = date("Y-m-d H:i:s", strtotime("+5 minutes"));
-
-            $userModel->deleteResetTokenByEmail($email);
-            $userModel->createResetToken($email, $token, $expires);
-
-            $resetLink = "http://localhost/Disaster-backend/controllers/resetPassword.php?token=$token";
-            $body = "
-                Hi {$user['username']},<br><br>
-                Click this link to reset your password:<br>
-                <a href='$resetLink'>$resetLink</a><br><br>
-                This link will expire in 5 minutes.
-            ";
-        }
-
-        // ✅ Send email using PHPMailer (reused)
         $mail = new PHPMailer(true);
         try {
             $mail->isSMTP();
@@ -55,7 +41,30 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             $mail->setFrom($_ENV['SMTP_EMAIL'], $_ENV['FROM_NAME']);
             $mail->addAddress($email);
             $mail->isHTML(true);
-            $mail->Subject = "Disaster Ready Password Reset";
+
+            if ($type === "create_account") {
+                // ✅ Account verification email
+                $verificationCode = rand(100000, 999999);
+                // Save code + expiry in DB if needed
+                $body = "Hi {$user['username']},<br><br>
+                         Your account verification code is: <b>$verificationCode</b><br>
+                         It will expire in 5 minutes.";
+                $mail->Subject = "Disaster Ready Account Verification";
+            } else {
+                // ✅ Forgot password email
+                $token = bin2hex(random_bytes(16));
+                $expires = date("Y-m-d H:i:s", strtotime("+5 minutes"));
+                $userModel->deleteResetTokenByEmail($email);
+                $userModel->createResetToken($email, $token, $expires);
+
+                $resetLink = "http://localhost/Disaster-backend/controllers/resetPassword.php?token=$token";
+                $body = "Hi {$user['username']},<br><br>
+                         Click this link to reset your password:<br>
+                         <a href='$resetLink'>$resetLink</a><br><br>
+                         This link will expire in 5 minutes.";
+                $mail->Subject = "Disaster Ready Password Reset";
+            }
+
             $mail->Body = $body;
             $mail->send();
 
@@ -70,6 +79,8 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
     echo json_encode([
         "success" => true,
-        "message" => "If this email exists, a reset message has been sent."
+        "message" => ($type === "create_account") 
+                     ? "Verification email sent." 
+                     : "If this email exists, a reset link has been sent."
     ]);
 }
