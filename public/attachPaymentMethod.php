8f37835dd4c9748c3afc390ab4641c2d80791384
Jared | Dev <jaredabrera44@gmail.com>
2025-10-26 16:10:06 +0800

    dfjsbsdfn

--- public/attachPaymentMethod.php
+++ public/attachPaymentMethod.php
@@ -6,8 +6,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 if($_SERVER['REQUEST_METHOD'] == 'OPTIONS'){
-      http_response_code(200);
-    exit();
+    
     
 // Get input
 $input = json_decode(file_get_contents("php://input"), true);
