11c18c2afbed3a6fb50f89fe19f76773985cee67
Jared | Dev <jaredabrera44@gmail.com>
2025-10-26 16:13:12 +0800

    ashkbaskdas

--- public/attachPaymentMethod.php
+++ public/attachPaymentMethod.php
@@ -5,7 +5,7 @@ header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
-if($_SERVER['REQUEST_METHOD'] == 'OPTIONS'){
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
     
     
 // Get input
