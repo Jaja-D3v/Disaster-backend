c169c889530f1174c4140014a9cf2560b0a1e37a
Jared | Dev <jaredabrera44@gmail.com>
2025-11-03 09:56:44 +0800

    Working frontend

--- public/getAdvisories.php
+++ public/getAdvisories.php
@@ -1,6 +1,7 @@
 <?php
 
 header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Content-Type: application/json");
@@ -11,5 +12,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     exit;
 }
 
- 
+require_once "../controllers/Auth.php"; 
 require_once "../controllers/getAdvisoryController.php";
